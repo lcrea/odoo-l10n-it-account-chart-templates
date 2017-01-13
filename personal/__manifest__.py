335132da348929fe9abac91157b614af9e22ee71
Luca Crea <lucacrea@yahoo.it>
2017-01-13 12:44:48 +0100

    Adaptation to Odoo v10
    [NEW]
    + Added OpenOffice source files as useful tools to change data files faster.
    
    [CHANGES]
    - Adapted accounts to the new version, removing view fields ("Mastrini") and adding user_type.
    - Leveraged new account tags object to implement previous view fields ("Mastrini").
    - Converted almost every data file to XML.
    - Moved any copyright reference, places all around, to the LICENSE file.

---
+++ personal/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Italia - Piano dei conti (personale)',
+    'version': '0.2',
+    'category': 'Localization/Account Charts',
+    'depends': [
+        'base_vat',
+        'base_iban',
+        'account_accountant',
+    ],
+    'author': 'Luca Crea',
+    'website': 'https://github.com/lcrea/odoo-l10n-it-account-chart-templates',
+    'description': """
+Piano dei conti italiano
+========================
+
+Fornisce un piano dei conti sufficientemente completo, comprensivo delle impostazioni base, per gestire
+la propria contabilità individuale.
+""",
+    'data': [
+        'data/account_chart_template_data_1.xml',
+        'data/account_account_tag_data.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
+        'data/account_chart_template_data_2.xml',
+        'data/account_chart_template_data.yml',
+        ],
+}
