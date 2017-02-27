efb32c932671ca109aef3c50e33731eff5414a16
Luca Crea <lucacrea@yahoo.it>
2017-02-27 18:55:40 +0100

    Account Template reviewed + Some fixes
    [NEW]
    + A new credit and debit account to handle personal invoices (phones, electrics, …).
    + Added a couple of account types to have more precise reports.
    
    [FIXES]
    - Improved account_chart default settings.
    - Reviewed the user type of many accounts.
    - Simplified balance accounts.

--- personal/__manifest__.py
+++ personal/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Italia - Piano dei conti (personale)',
-    'version': '0.2',
+    'version': '0.3',
     'category': 'Localization/Account Charts',
     'depends': [
         'base_vat',
         'base_iban',
         'account_accountant',
+        'account_cancel',
     ],
     'author': 'Luca Crea',
     'website': 'https://github.com/lcrea/odoo-l10n-it-account-chart-templates',
@@ -20,6 +21,7 @@ la propria contabilità individuale.
     'data': [
         'data/account_chart_template_data_1.xml',
         'data/account_account_tag_data.xml',
+        'data/account_type_data.xml',
         'data/account.account.template.csv',
         'data/account_tax_template_data.xml',
         'data/account_fiscal_position_template_data.xml',
